6f156a2b4db73b84f7969a07704335bc0f3c660b
MD. SYMUM HOSSAIN <69222900+SymumAust@users.noreply.github.com>
2024-04-14 03:19:22 +0600

    part-3

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+def home(request):
+    return render(request, "index.html")
+
+
+
+def success_page(request):
+    print('*'*10)
+    return HttpResponse("<h1>Hey I'm Django</h1>")
