005341f6c1fda69ac1add1701f33a8a00211a63f
MD. SYMUM HOSSAIN <69222900+SymumAust@users.noreply.github.com>
2024-04-16 02:49:57 +0600

    done almost

--- core/home/views.py
+++ core/home/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def home(request):
-    return render(request, "index.html")
+    peoples =[
+        {'name': 'John', 'age': '16' },
+        {'name': 'Robin', 'age': '86' },
+        {'name': 'Ej', 'age': '32' },
+        {'name': 'ek', 'age': '87' }
+
+    ]
+    for people in peoples:
+        print(people)
+
+    return render(request, 'index.html', context = {'peoples': peoples})
 
 
 
